b3c44cce5176bd8b437f74f73c806e7df0dac103
Nipun Malinga <nipunmalinga11@gmail.com>
2025-04-28 00:41:35 +0530

    fix: fix import issues

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router';
 import { Provider } from './components/ui/provider.jsx';
-import router from './router.js';
+import router from './router.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
